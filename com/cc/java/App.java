0052ca55b9103e0937aa733fa342ac474ced88fc
StepEnne <eins23stephan@googlemail.com>
2024-01-24 11:44:09 +0100

    Cat1/Cat2++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,37 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+    //Objekt entsteht
+    Cat cat = new Cat();        //Instanziierung, erzeuge aus einer klasse ein objekt
+    //Wertzuweisung
+    cat.name = "Alonzo";
+    cat.furcolor = "grey";
+    cat.age = 35;
+    
+//    cat.tellYourAdress();
+    //    cat.tellYourName();   
+    System.out.println(cat.name);
+    System.out.println(cat.furcolor);
+    System.out.println(cat.age);
 
+    System.out.println("----------------------------");
 
+    Cat cat2 = new Cat();
+
+    cat2.name = "Grizabella";
+    cat2.furcolor = "white";
+    cat2.age = 29;
+    
+    System.out.println(cat2.name);
+    System.out.println(cat2.furcolor);
+    System.out.println(cat2.age);
     }
 
-   
+//    cat.tellYourName();      
+      Cat cat2 = new Cat();
+//    System.out.println(cat2);
+//    cat2.tellYourAdress();
+  
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,23 @@
+package com.cc.java;
+
+public class Cat {
+
+  
+
+String name;
+String furcolor;
+int age;
+
+public void tellYourName(){
+
+System.out.println(this.name);
+
+}
+
+
+    public void tellYourAdress(){
+        System.out.println(this);
+
+    }
+
+}
